71078d68245811b01a35834e357dcbda17e252d5
Hw7aCvm <125972306+Hw7aCvm@users.noreply.github.com>
2024-01-01 07:41:00 +0800

    +calendar & events panel

--- frontend-reactjs/src/App.js
+++ frontend-reactjs/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import CalendarMain from "./components/CalendarMain";
+import EventsPanel from "./components/EventsPanel";
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="d-flex flex-row">
+            <div id="eventsPanel">
+                <EventsPanel/>
+            </div>
+            <div className='flex-grow-1'>
+                <CalendarMain/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
